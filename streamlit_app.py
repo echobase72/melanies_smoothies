c05c511a664f8ee6b840a00069bb7d900ac8ab02
echobase72 <paul.ransley72@gmail.com>
2024-09-30 12:17:59 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
+import Pandas as pd
 
 # Write directly to the app
 st.title("Customize Your Smoothie!")
